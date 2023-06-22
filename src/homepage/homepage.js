e79d147364f2ce460bba9d9f940de94007f2c4a5
kundan kumar <kundansingh023230@gmail.com>
2023-06-22 09:49:49 +0530

     

--- src/homepage/homepage.js
+++ src/homepage/homepage.js
@@ -39,7 +39,7 @@ const deleteTask=(tasktoDelete)=>{
     </ul>
     
     </div>
-    <button onClick={() => navigate('/register')}> Logout </button>
+    <button className="Logout" onClick={() => navigate('/register')}> Logout </button>
     </>
   )
 }
